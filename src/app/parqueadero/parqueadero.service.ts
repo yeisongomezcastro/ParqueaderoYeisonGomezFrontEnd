e1ed01626b8d13c467e5ee4f233a90575b3a179d
Yeison Arley Gómez - Ceiba Software <yeison.gomez@ceiba.com.co>
2018-05-25 10:38:18 -0500

    final

--- src/app/parqueadero/parqueadero.service.ts
+++ src/app/parqueadero/parqueadero.service.ts
@@ -13,11 +13,11 @@ export class ComponentService {
   }
 
   consultar(placa:String){
-    return  this.httpClient.get('http://localhost:8080/servicio/buscarvehiculo/' + placa).toPromise();
+    return  this.httpClient.get('http://localhost:8080/servicio/registrarsalidavehiculo/' + placa).toPromise();
   }
 
   guardar(vehiculo:Vehiculo){
-    return  this.httpClient.post<any>('http://localhost:8080/servicio/guardar', vehiculo).toPromise();
+    return  this.httpClient.post<any>('http://localhost:8080/servicio/registraringresovehiculo', vehiculo).toPromise();
   }
 
 }
